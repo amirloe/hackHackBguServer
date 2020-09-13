bb292b600c10263610213717a826b52f0417b8fd
amirloe <amirloe@post.bgu.ac.il>
2020-09-13 11:11:39 +0300

    Initial commit

---
+++ src/main/java/com/hackbgu/hackBguServer/HackBguServerApplication.java
@@ -0,0 +1,13 @@
+package com.hackbgu.hackBguServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackBguServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackBguServerApplication.class, args);
+	}
+
+}
